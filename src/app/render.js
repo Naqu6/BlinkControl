501420fd9b449648686988706ce4c01a834e1c4b
Ryan Ehrlich <ryansehrlich@gmail.com>
2019-05-25 21:06:36 -0700

    Initial app code

---
+++ src/app/render.js
@@ -0,0 +1,16 @@
+import cv from "../build/Release/module";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
+
+
+// cv.startProcessing((left, right) => {
+// 	console.log(left, right);
+
+// 	document.querySelector()
+// });
