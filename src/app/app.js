9ad8f9c2e37f8e3e4c72d6339074b4dd7925a512
Ryan Ehrlich <ryansehrlich@gmail.com>
2019-05-26 20:54:16 -0700

    Initial implementation of BlinkController

--- src/app/app.js
+++ src/app/app.js
@@ -3,8 +3,40 @@ import BlinkController from "../app/controllers/blinkController";
 
 export default class App extends React.Component {
 	render() {
+		var options = {
+			displayText: "root",
+			binaryChoice: false,
+			final: false,
+			options: [{
+				binaryChoice: true,
+				final: false,
+				displayText: "Start",
+				options: {
+					blinkingChoice: {
+						displayText: "Blink for this choice",
+						binaryChoice: false,
+						final: true,
+						options: [{
+							displayText: "You blink!"
+						}, {
+							displayText: "Back",
+						}]
+					}, notBlinkingChoice: {
+						displayText: "Don't blink for this choice",
+						binaryChoice: false,
+						final: true,
+						options: [{
+							displayText: "You didn't blink!"
+						}, {
+							displayText: "Back",
+						}]
+					}
+				}
+			}]
+		};
+
 		return (
-			<BlinkController />
+			<BlinkController decisionTime={500} values={options}/>
 		);
 	}
 }
--- src/app/controllers/blinkController.js
+++ src/app/controllers/blinkController.js
@@ -1,42 +1,221 @@
 import cv from "../../build/Release/blinkDetector";
 import React from "react";
 
+function choice(props) {
+	return (
+		<div className="choice">
+			<div className={`choice-name ${props.highlighted ? "highlighted": ""}`}>
+				{props.name}
+			</div>
+		</div>
+	);
+
+}
+
+
+function BinaryOptions(props) {
+	return (
+		<div className="options binary-options"> 
+			<div>
+				Blink Options: {props.current.options.blinkingChoice.displayText}
+			</div>
+
+			<br/>
+
+			<div>
+				No-Blink Options: {props.current.options.notBlinkingChoice.displayText}
+			</div>
+		</div>
+	);
+}
+
+function MultipleOptions(props) {
+	return (
+		<div className="options multiple-options">
+			{
+				props.current.options.map((option, index) => {
+					if (index == props.highlightedIndex) {
+						return (
+							<div>
+								<b>
+									{option.displayText}
+								</b>
+							</div>
+						);
+					} else {
+						return (
+							<div>
+								{option.displayText}
+							</div>
+						);
+					}
+				})
+			}
+		</div>
+	);
+}
+
+function Options(props) {
+	if (props.current.binaryChoice) {
+		return BinaryOptions(props);
+	} else {
+		return MultipleOptions(props);
+	}
+}
+
+
 export default class BlinkController extends React.Component {
+	// Abstraction for blink controller
+
 	constructor(props) {
+		/* 
+			Props:
+			{
+				values: {
+					final: boolean,
+					Display Text: string,
+					Options: [this schema],
+
+					// if final == true
+					Callback: action
+				},
+					
+				decisionTime: float, // in millis
+			}
+		*/
+
 		super(props);
+
+		const startTime = Date.now();
 
 		this.state = {
 			leftEyeRatio: null,
 			rightEyeRatio: null,
-			startTime: Date.now(),
-			aselected: true,
-			running: true
+			path: [],
+			highlightedIndex: 0,
+			blinkStartTime: startTime,
+			updateTime: startTime,
+			running: true,
 		}
 
 		this.start = this.start.bind(this);
-		this.stop = this.start.bind(this);
+		this.stop = this.stop.bind(this);
+
+		this.processData = this.processData.bind(this);
+		this.getCurrent = this.getCurrent.bind(this);
+		this.updateBlinkData = this.updateBlinkData.bind(this);
+	}
+
+	getCurrent() {
+		var current = this.props.values; 
+
+		this.state.path.forEach((key) => {
+			current = current.options[key];
+		});
+
+		return current;
+	}
+
+	getBlinkStatus(leftEyeRatio, rightEyeRatio) {
+		if (leftEyeRatio < 0.3 && rightEyeRatio < 0.3) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	updateBlinkData(leftEyeRatio, rightEyeRatio) {
+		this.setState({
+			leftEyeRatio: parseFloat(leftEyeRatio),
+			rightEyeRatio: parseFloat(rightEyeRatio),
+		});
+	}
+
+	runAction(blinking, updateTime) {
+		var options = this.getCurrent();
+		var key;
+
+		if (options.binaryChoice) {
+			key = blinking ? "blinkingChoice" : "notBlinkingChoice";
+		} else {
+			if (!blinking) {
+				var highlightedIndex = this.state.highlightedIndex + 1;
+
+				if (highlightedIndex >= options.options.length) {
+					highlightedIndex = 0;
+				}
+
+				this.setState({
+					highlightedIndex: highlightedIndex
+				});
+
+				this.setState({
+					updateTime: updateTime,
+				});
+
+				return;
+			}
+
+			key  = this.state.highlightedIndex;
+		}
+
+		var callback = options.options[key].callback;
+		
+		if (callback) {
+			callback();
+		};
+
+		var path = [];
+
+		if (!options.final) {
+			path = [...this.state.path, key];
+		}
+
+		this.setState({
+			path: path
+		});
+
+		this.setState({
+			updateTime: updateTime + this.props.decisionTime,
+			blinkStartTime: null
+		});
+
+	}
+
+	processData(leftEyeRatio, rightEyeRatio) {
+		let currentTime = Date.now();
+
+		this.updateBlinkData(leftEyeRatio, rightEyeRatio)
+
+		var timeDelta = currentTime - this.state.updateTime;
+
+		if (timeDelta < 0) {
+			return;
+		}
+		
+		var blinking = this.getBlinkStatus(leftEyeRatio, rightEyeRatio);
+
+		if (blinking) {
+			if (!this.state.blinkStartTime) {
+				this.setState({
+					blinkStartTime: currentTime
+				});
+			} else if (currentTime - this.state.blinkStartTime >= this.props.decisionTime) {
+				this.runAction(true, currentTime);
+			}
+		} else {
+			this.setState({
+				blinkStartTime: null
+			});
+
+			if (timeDelta >= this.props.decisionTime) {
+				this.runAction(false, currentTime);
+			}
+		}
 	}
 
 	start() {
-		cv.start((leftEyeRatio, rightEyeRatio) => {
-			let currentTime = Date.now();
-
-			if (this.state.running) {
-				if (leftEyeRatio < 0.3 && rightEyeRatio < 0.3) {
-					this.setState({running: false})
-				} else if (currentTime - this.state.startTime > 500) {
-					this.setState({
-						aselected: !this.state.aselected,
-						startTime: currentTime,
-					});
-				}
-			}
-
-			this.setState({
-				leftEyeRatio: parseFloat(leftEyeRatio),
-				rightEyeRatio: parseFloat(rightEyeRatio)
-			})			
-		});
+		cv.start(this.processData);
 	}
 
 	stop() {
@@ -54,10 +233,6 @@ export default class BlinkController extends React.Component {
 					<button onClick={this.stop}>
 						Stop
 					</button>
-
-					<button onClick={this.resetGame}>
-
-					</button>
 				</div>
 
 				<div>
@@ -68,14 +243,7 @@ export default class BlinkController extends React.Component {
 					Right Eye: {this.state.rightEyeRatio}
 				</div>
 
-				<div class="a">
-					A {this.state.aselected ? "- Selected" : ""}
-				</div>
-
-				<div class="b">
-					B {this.state.aselected ? "" : "- Selected"}
-				</div>
-
+				<Options current={this.getCurrent()} highlightedIndex={this.state.highlightedIndex} />
 			</div>
 		);
 	}
